4abf34ba06384606dee310add5557fd594ab465d
Matthew <m.martin747@gmail.com>
2019-02-08 11:00:13 +1000

    shortlist fix

--- src/api/init.js
+++ src/api/init.js
@@ -5,7 +5,8 @@ const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
       ? "https://wbgs-server.herokuapp.com"
-      : "http://localhost:3000",
+      :"https://wbgs-server.herokuapp.com",
+      // : "http://localhost:3000",
       // : "http://localhost:3000"
   headers: { "Content-Type": "application/json" },
   proxy: false,
